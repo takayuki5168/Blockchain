b34fb9c5aa9942c00426f812d225bd9ed9b01df6
Takayuki Murooka <takayuki5168@gmail.com>
2018-01-28 17:35:05 +0900

    add function of consensus algorithm

--- blockchain.py
+++ blockchain.py
@@ -4,11 +4,15 @@ from textwrap import dedent
 from time import time
 from uuid import uuid4
 from flask import Flask, jsonify, request
+from urllib.parse import urlparse
+import requests
 
 class Blockchain:
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+
+        self.nodes = set()
 
         # make the genesis block
         self.new_block(previous_hash = 1, proof = 100)
@@ -64,6 +68,58 @@ class Blockchain:
 
         return guess_hash[:4] == "0000"
 
+    def register_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    # check whether chain is valid
+    def valid_chain(self, chain):
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print('{}'.format(last_block))
+            print('{}'.format(block))
+            print('\n-------------\n')
+
+            # check whether hash is correct
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            # check whether PoW is correct
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
+
+    def resolve_conflicts(self):
+        new_chain = None
+
+        max_length = len(self.chain)
+
+        for node in self.nodes:
+            response = requests.get('http://{}/chain'.format(node))
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                # check whether the chain is the longest & valid
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
+
+
 
 # make new node
 app = Flask(__name__)
@@ -105,6 +161,7 @@ def mine():
     last_proof = last_block['proof']
     proof = blockchain.proof_of_work(last_proof)
 
+    # TODO transactionが無くてもmining自身のtransactionで採掘できてしまうバグ
     blockchain.new_transaction(
         sender = '0',
         recipient = node_identifire,
@@ -120,6 +177,40 @@ def mine():
         'proof' : block['proof'],
         'previous_hash' : block['previous_hash'],
         }
+    return jsonify(response), 200
+
+@app.route('/nodes/register', methods = ['POST'])
+def register_node():
+    values= request.get_json()
+
+    nodes = values.get('nodes')
+    if nodes is None:
+        return '[Error] the list of invalid node', 400
+
+    for node in nodes:
+        block_chain.register_node(node)
+
+    response = {
+        'message' : 'new node is added',
+        'total_nodes' : list(blockchain.nodes),
+        }
+    return jsonify(response), 201
+
+@app.route('/nodes/resolve', methods = ['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message' : 'chain is replaced',
+            'chain' : blockchain.chain,
+            }
+    else:
+        response = {
+            'message' : 'chain is checked',
+            'chain' : blockchain.chain,
+            }
+        
     return jsonify(response), 200
 
 # start a server using port 5000
